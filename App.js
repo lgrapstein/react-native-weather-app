029c8f7cb213ebbfec3d1e8fb65f4e96e6262c35
Lauren Grapstein <laurengrapstein@Laurens-MBP.home>
2018-01-31 10:45:19 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
+
+
+// ActivityIndicator is a built-in component that displays a circular loading spinner. We’ll use it when data is being fetched from the network
+// StatusBar is a built-in component that allows us to modify the app status bar at the top of the device
+
+import { fetchLocationId, fetchWeather } from './utils/api';
+import getImageForWeather from './utils/getImageForWeather';
+
+import SearchInput from './components/SearchInput';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+// The loading property represents when a call is still being made (in order to show a loading icon) and error is used to store the error message if our call fails or returns unusable information.
+
+    this.state = {
+      loading: false,
+      error: false,
+      location: '',
+      temperature: 0,
+      weather: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('New York');
+  }
+
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          temperature,
+          weather,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    })
+  }
+
+  render() {
+    const { loading, error, location, weather, temperature } = this.state;
+
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>{weather}</Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>{`${Math.round(temperature)}°`}</Text>
+                  </View>
+                )}
+              </View>
+            )}
+            <SearchInput
+              placeholder="Search any city"
+              onSubmit={this.handleUpdateLocation}
+            />
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+});
---
+++ utils/api.js
@@ -0,0 +1,23 @@
+// Returns an array of locations based on a search query
+export const fetchLocationId = async city => {
+  const response = await fetch(
+    `https://www.metaweather.com/api/location/search/?query=${city}`,
+  );
+  const locations = await response.json();
+  return locations[0].woeid;
+};
+
+// Returns weather details about a specific location using a location identifier
+export const fetchWeather = async woeid => {
+  const response = await fetch(
+    `https://www.metaweather.com/api/location/${woeid}/`,
+  );
+  const { title, consolidated_weather } = await response.json();
+  const { weather_state_name, the_temp } = consolidated_weather[0];
+
+  return {
+    location: title,
+    weather: weather_state_name,
+    temperature: the_temp,
+  };
+};
